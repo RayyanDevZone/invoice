883ba65eb4181599282ac1b505293b46e1ae40eb
RayyanDevZone <rayyansyed369@gmail.com>
2024-12-11 18:23:04 +0530

    contact details in invoice added

--- src/components/Invoice/Invoice.jsx
+++ src/components/Invoice/Invoice.jsx
@@ -9,36 +9,62 @@ const Invoice = () => {
       <div className="logoAndAddress w-full h-[250px] flex flex-row items-center justify-between">
         <div className="logo h-[150px] w-[50%] flex justify-center items-start px-6 flex-col">
           {invoiceData.logo && <img src={invoiceData.logo} alt="Logo" />}
-          <h1 className="text-xl font-bold text-blue-600">{invoiceData.sender.name || "Company Name"}</h1>
+          <h1 className="text-xl font-bold text-blue-600">
+            {invoiceData.sender.name || "Company Name"}
+          </h1>
         </div>
         <div className="address w-[50%] h-[100%] flex items-end justify-evenly flex-col px-6">
           <div className="invoiceAndNumber flex flex-col items-end justify-center">
-            <h1 className="text-3xl font-bold font-poppins text-gray-800">Invoice</h1>
-            <h1 className="text-xl text-gray-500 font-semibold">#{invoiceData.invoiceNumber}</h1>
+            <h1 className="text-3xl font-bold font-poppins text-gray-800">
+              Invoice
+            </h1>
+            <h1 className="text-xl text-gray-500 font-semibold">
+              #{invoiceData.invoiceNumber}
+            </h1>
           </div>
           <div className="address flex flex-col items-end text-gray-700 text-[18px] font-semibold font-poppins justify-center">
             <p>{invoiceData.sender.address}</p>
-            <p>{invoiceData.sender.city}, {invoiceData.sender.zip}</p>
+            <p>
+              {invoiceData.sender.city}, {invoiceData.sender.zip}
+            </p>
             <p>{invoiceData.sender.country}</p>
           </div>
         </div>
       </div>
       <div className="RecieverAddressDate w-full h-[200px] flex flex-row items-center justify-between px-6">
         <div className="BillTo w-[50%] h-full">
-          <h1 className="text-2xl font-bold text-gray-800 font-poppins">Bill to:</h1>
-          <p className="text-xl font-bold text-gray-800">{invoiceData.receiver.name}</p>
-          <p className="text-gray-600 font-semibold">{invoiceData.receiver.address}</p>
-          <p className="text-gray-600 font-semibold">{invoiceData.receiver.city}, {invoiceData.receiver.zip}</p>
-          <p className="text-gray-600 font-semibold">{invoiceData.receiver.country}</p>
+          <h1 className="text-2xl font-bold text-gray-800 font-poppins">
+            Bill to:
+          </h1>
+          <p className="text-xl font-bold text-gray-800">
+            {invoiceData.receiver.name}
+          </p>
+          <p className="text-gray-600 font-semibold">
+            {invoiceData.receiver.address}
+          </p>
+          <p className="text-gray-600 font-semibold">
+            {invoiceData.receiver.city}, {invoiceData.receiver.zip}
+          </p>
+          <p className="text-gray-600 font-semibold">
+            {invoiceData.receiver.country}
+          </p>
         </div>
         <div className="invoiceDate w-[50%] h-full flex flex-col justify-start items-end">
           <div className="flex flex-row w-auto mt-6">
-            <h1 className="text-l font-bold text-gray-800 font-poppins">Invoice Date:</h1>
-            <p className="text-gray-500 font-semibold px-3">{invoiceData.issueDate}</p>
+            <h1 className="text-l font-bold text-gray-800 font-poppins">
+              Invoice Date:
+            </h1>
+            <p className="text-gray-500 font-semibold px-3">
+              {invoiceData.issueDate}
+            </p>
           </div>
           <div className="flex flex-row w-auto">
-            <h1 className="text-l font-bold text-gray-800 font-poppins">Due Date:</h1>
-            <p className="text-gray-500 font-semibold px-3">{invoiceData.dueDate}</p>
+            <h1 className="text-l font-bold text-gray-800 font-poppins">
+              Due Date:
+            </h1>
+            <p className="text-gray-500 font-semibold px-3">
+              {invoiceData.dueDate}
+            </p>
           </div>
         </div>
       </div>
@@ -49,7 +75,9 @@ const Invoice = () => {
               <th className="text-gray-700 font-bold p-1 text-center">Items</th>
               <th className="text-gray-700 font-bold p-1 text-center">Qty</th>
               <th className="text-gray-700 font-bold p-1 text-center">Rate</th>
-              <th className="text-gray-700 font-bold p-1 text-center">Amount</th>
+              <th className="text-gray-700 font-bold p-1 text-center">
+                Amount
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -84,13 +112,26 @@ const Invoice = () => {
         <p className="mt-2">{invoiceData.paymentTerms}</p>
       </div>
       <div className="accountDetails h-36 w-full px-6 mt-6">
-        <p className="font-bold text-gray-800">Please send the payment to these details:</p>
-        <p className="font-semibold text-sm text-gray-800">Bank: {invoiceData.paymentInfo?.bankName}</p>
-        <p className="font-semibold text-sm text-gray-800">Account Name: {invoiceData.paymentInfo?.accountName}</p>
-        <p className="font-semibold text-sm text-gray-800">Account Number: {invoiceData.paymentInfo?.accountNumber}</p>
+        <p className="font-bold text-gray-800">
+          Please send the payment to these details:
+        </p>
+        <p className="font-semibold text-sm text-gray-800">
+          Bank: {invoiceData.paymentInfo?.bankName}
+        </p>
+        <p className="font-semibold text-sm text-gray-800">
+          Account Name: {invoiceData.paymentInfo?.accountName}
+        </p>
+        <p className="font-semibold text-sm text-gray-800">
+          Account Number: {invoiceData.paymentInfo?.accountNumber}
+        </p>
       </div>
       <div className="contactDetails h-auto w-full px-6">
-        <p className="text-gray-600">If you have any questions concerning this invoice, use the following contact information:</p>
+        <p className="text-gray-600">
+          If you have any questions concerning this invoice, use the following
+          contact information:
+        </p>
+        <p className="text-gray-600">Phone: {invoiceData.sender.phone}</p>{" "}
+        <p className="text-gray-600">Email: {invoiceData.sender.email}</p>
       </div>
     </div>
   );
